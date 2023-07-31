328a6222960715eb3c2f24c4a7bed1ce1be2cfd6
FullStackEng <fullstackeng@outlook.com>
2023-08-01 00:27:29 +0800

    Add code to get latest kb for component

--- get_component_for_checked_kb.py
+++ get_component_for_checked_kb.py
@@ -1,7 +1,6 @@
 # check whether there is duplicate kb number.
 # find the component corresponding for the kb number.
 # get latest update for the component.
-# compare with previous kb number, if same, just download. if not, download latest one.
 
 import os
 import pathlib
@@ -16,11 +15,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def add_kb_to_list(logger, checked_kb_list, kb_number_str):
-    if kb_number_str in checked_kb_list:
+def add_kb_to_list(logger, kb_list_checked, kb_number_str):
+    if kb_number_str in kb_list_checked:
         logger.info("Duplicate KB number: " + kb_number_str)
     else:
-        checked_kb_list.append(kb_number_str)
+        kb_list_checked.append(kb_number_str)
 
 
 current_script_folder = str(pathlib.Path(__file__).parent.absolute()) + os.sep
@@ -72,9 +71,9 @@ try:
 
             add_kb_to_list(logger, checked_kb_list, kb_number_str)
 
-    logger.info("Read checked_kb_list file, length is: " + str(len(checked_kb_list)))
-except:
-    logger.info("Encounter exception when loading expected kb list.")
+    logger.info("Read kb_list_checked file, length is: " + str(len(checked_kb_list)))
+except Exception as ex:
+    logger.info("Encounter exception when loading kb_list_checked file." + str(ex))
 finally:
     f.close()
 
@@ -85,7 +84,6 @@ browser = webdriver.Edge()
 
 for kb in checked_kb_list:
     kb_str = kb.strip().upper()
-
     logger.info("KB number: " + kb_str)
 
     kb_number = re.sub("KB", "", kb_str)
@@ -126,9 +124,23 @@ for kb in checked_kb_list:
             kb_component_error_list.append(kb_str + ", ")
 
     if package_name is not None:
-        logger.info("Package name: " + package_name.text)
+        package_name_str = package_name.text
 
-        kb_component_list.append(kb_str + "," + package_name.text)
+        logger.info("Package name: " + package_name_str)
+
+        # Find the index of the first "-"
+        first_dash_index = package_name_str.find("-")
+
+        if first_dash_index > 0:
+            # Extract the substring before the first "-"
+            first_substring = package_name_str[:first_dash_index]
+
+            # Split the last four characters
+            component_name = first_substring[:-4]
+        else:
+            component_name = package_name_str
+
+        kb_component_list.append(kb_str + "," + package_name_str + "," + component_name)
     else:
         logger.error("Can't find component name for " + kb_str)
         browser.save_screenshot(
@@ -140,12 +152,17 @@ browser.quit()
 logger.info("kb_list_component count: " + str(len(kb_component_list)))
 logger.info("kb_list_component_error count: " + str(len(kb_component_error_list)))
 
-kb_component_file = current_script_folder + "kb_list_component.txt"
+
+kb_component_list.sort()
+kb_component_file = current_script_folder + "output_component_for_checked_kb.txt"
 with open(kb_component_file, "w") as f:
     for item in kb_component_list:
         f.write("%s\n" % item)
 
-kb_component_error_file = current_script_folder + "kb_list_component_error.txt"
+kb_component_error_list.sort()
+kb_component_error_file = (
+    current_script_folder + "output_component_for_checked_kb_error.txt"
+)
 with open(kb_component_error_file, "w") as f:
     for item in kb_component_error_list:
         f.write("%s\n" % item)
---
+++ get_latest_kb_for_component.py
@@ -0,0 +1,88 @@
+import os
+import pathlib
+import shutil
+from pathlib import Path
+
+from my_logger_object import create_logger_object
+
+
+def copy_component(component_kb_list, component_name, source_folder, target_folder):
+    # source_folder = r"C:\CodeRepos\GetOfficeKBs\Folder_Office2016_KBs\x64_msp"
+    # target_folder = r"C:\CodeRepos\GetOfficeKBs\Folder_Latest_KB_Numbers\x64_msp"
+    # component_name = ""
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    for root, dirs, files in os.walk(source_folder):
+        for file_name in files:
+            component_name_in_file = file_name.split("-")[0].strip()
+            if component_name == component_name_in_file:    
+                soure_file_path = root + os.sep + file_name
+                target_file_path = target_folder + os.sep + file_name
+
+                kb_number_in_file = file_name.split("_")[1].strip()
+
+                if (component_name + "," + kb_number_in_file) not in component_kb_list:
+                    component_kb_list.append(component_name + "," + kb_number_in_file)
+
+                if os.path.isfile(soure_file_path):
+                    try:
+                        shutil.copy(soure_file_path, target_file_path)
+                    except:
+                        logger.debug("exception")
+
+
+current_script_folder = str(pathlib.Path(__file__).parent.absolute()) + os.sep
+
+FILENAME = current_script_folder + "log_" + os.path.basename(__file__) + ".log"
+logger = create_logger_object(FILENAME)
+
+logger.info("The script starts running.")
+logger.info("The script folder is " + current_script_folder)
+
+component_list = []
+try:
+    f = open(current_script_folder + "output_component_for_checked_kb.txt", "r")
+
+    for line in f:
+        component_str = line.split(",")[-1].strip()
+
+        if component_str in component_list:
+            logger.info("Duplicate component number: " + component_str)
+        else:
+            component_list.append(component_str)
+except Exception as ex:
+    logger.info("Encounter exception when loading expected kb list." + str(ex))
+finally:
+    f.close()
+
+logger.info(len(component_list))
+
+component_list.sort()
+component_list_file = current_script_folder + "output_non_dup_component.txt"
+with open(component_list_file, "w") as f:
+    for item in component_list:
+        f.write("%s\n" % item)
+
+source_folder_x32 = r"C:\CodeRepos\GetOfficeKBs\Folder_Office2016_KBs\x86_msp"
+target_folder_x32 = r"C:\CodeRepos\GetOfficeKBs\Folder_Latest_KB_Numbers\x86_msp"
+
+source_folder_x64 = r"C:\CodeRepos\GetOfficeKBs\Folder_Office2016_KBs\x64_msp"
+target_folder_x64 = r"C:\CodeRepos\GetOfficeKBs\Folder_Latest_KB_Numbers\x64_msp"
+
+
+component_kb_list = []
+
+for item in component_list:
+    logger.debug(item)
+
+    copy_component(component_kb_list, item, source_folder_x32, target_folder_x32)
+    copy_component(component_kb_list, item, source_folder_x64, target_folder_x64)
+
+component_kb_list.sort()
+
+component_kb_list_file = current_script_folder + "output_latest_kb_for_component.txt"
+with open(component_kb_list_file, "w") as f:
+    for item in component_kb_list:
+        f.write("%s\n" % item)
